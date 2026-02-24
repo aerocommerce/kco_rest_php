658bfb67105e0c46919b0a0c70d9e8ef31c5e624
Mike Price <michael.price@aerocommerce.com>
2026-02-24 10:40:12 +0000

    revert CURLConnector change

--- src/Klarna/Rest/Transport/CURLConnector.php
+++ src/Klarna/Rest/Transport/CURLConnector.php
@@ -268,7 +268,7 @@ class CURLConnector implements ConnectorInterface
     public static function create(
         $merchantId,
         $sharedSecret,
-        $baseUrl = self::BASE_URL,
+        $baseUrl = self::EU_BASE_URL,
         UserAgentInterface $userAgent = null
     ) {
         return new static($merchantId, $sharedSecret, $baseUrl, $userAgent);
